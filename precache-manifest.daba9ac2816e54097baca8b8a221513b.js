e3896c95b7d93ddd674334f652b65f73a8426089
josprins <jco.prins@gmail.com>
2020-04-14 13:58:54 +0200

    Updates

---
+++ precache-manifest.daba9ac2816e54097baca8b8a221513b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef5ad8ead16abc809af089dff347c7b",
+    "url": "/thistodo/index.html"
+  },
+  {
+    "revision": "3627c8038a1e7226c5be",
+    "url": "/thistodo/static/css/main.d02cecf1.chunk.css"
+  },
+  {
+    "revision": "ae3093e649a2f0654b21",
+    "url": "/thistodo/static/js/2.9b077270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thistodo/static/js/2.9b077270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3627c8038a1e7226c5be",
+    "url": "/thistodo/static/js/main.2ea3e4e4.chunk.js"
+  },
+  {
+    "revision": "9fa8ad665c8589c4a95d",
+    "url": "/thistodo/static/js/runtime-main.2ea78618.js"
+  }
+]);
